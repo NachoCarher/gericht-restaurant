fa8b255c8dcfcef667765e3240d624ab847cc279
Nacho Carvajal Herrera <nacho.carvajal@unit4.com>
2023-01-20 17:26:44 +0100

    Created header component and style

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+import { images } from '../../constants';
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className='p__cormorant'>{title}</p>
+    <img src={images.spoon} alt='spoon' className='spoon_image'/>
+    <p className='p__opensans' style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+    <button type="button" className='custom__button'>Explore menu</button>
   </div>
 );
 
