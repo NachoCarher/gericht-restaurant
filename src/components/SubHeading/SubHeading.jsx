262fb7ef587ee979a67288b3f657a39b3ad71849
Nacho Carvajal Herrera <nacho.carvajal@unit4.com>
2023-01-24 13:39:13 +0100

    Created menu component and style. Fixed about typo

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -6,7 +6,7 @@ const SubHeading = ({ title }) => (
   <div style={{ marginBottom: '1rem' }}>
     <p className='p__cormorant'>{title}</p>
     <img src={images.spoon} alt='spoon' className='spoon_image'/>
-    <p className='p__opensans' style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+    <p className='p__opensans' style={{ margin: '2rem 0' }}>{title}</p>
     <button type="button" className='custom__button'>Explore menu</button>
   </div>
 );
