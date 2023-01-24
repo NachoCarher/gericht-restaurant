42928bfe7cb76241df405a38b0f6ae7ea6143623
Nacho Carvajal Herrera <nacho.carvajal@unit4.com>
2023-01-24 14:22:28 +0100

    Created chef component and style

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -5,9 +5,7 @@ import { images } from '../../constants';
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: '1rem' }}>
     <p className='p__cormorant'>{title}</p>
-    <img src={images.spoon} alt='spoon' className='spoon_image'/>
-    <p className='p__opensans' style={{ margin: '2rem 0' }}>{title}</p>
-    <button type="button" className='custom__button'>Explore menu</button>
+    <img src={images.spoon} alt='spoon' className='spoon__img'/>
   </div>
 );
 
